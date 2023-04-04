40d02881accc339d017df9b6c5b437e5f8f73e21
Emilse Arias <emilsearias@gmail.com>
2023-04-04 10:16:49 -0300

    agregar un nuevo nodo hijo

--- js/app.js
+++ js/app.js
@@ -29,5 +29,12 @@ function verMas(){
     console.log(seccion);
     //crear un nodo hijo
     let parrafo = document.createElement('p');
-    console.log(parrafo);
+    //preparar el elemento
+    parrafo.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id perferendis neque assumenda dicta, quo debitis, est cum possimus nisi et officia sapiente voluptatum ex, aperiam repudiandae voluptatibus harum accusantium quos molestiae dolorum temporibus? Sequi ex doloremque quae, cum voluptatum excepturi vitae possimus at veritatis, unde eaque asperiores autem repudiandae, deserunt et illo aut debitis laborum accusamus quos soluta? Et molestias excepturi deserunt odio, debitis inventore nisi magni iure totam reiciendis eligendi. Quibusdam dolor ab sint sit facilis perferendis iure ipsam veniam impedit, dolore, eum, ut voluptatem enim minus perspiciatis possimus ipsa? Saepe modi vitae esse distinctio voluptatibus quia atque labore fuga odit. Iusto optio magnam ut, quam amet molestiae nisi. Quaerat, ullam ad! Magnam recusandae accusantium amet iusto explicabo laborum excepturi inventore maiores molestiae iste accusamus assumenda magni similique reiciendis repellat, eaque, error, at consectetur voluptatem maxime quas. Tempore dolor autem alias delectus perspiciatis, repudiandae sit sint facilis cupiditate velit.'
+    parrafo.className = 'lead';
+   //agregar el nodo hijo en la seccion padre
+   //seccion.appendChild(parrafo); //agrega un nodo al final
+   //seccion.prepend(parrafo); //permite agregar un nodo al principio
+   seccion.insertBefore(parrafo,botonVerMas);
+
 }
